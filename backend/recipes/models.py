098b990d1f99f131085b833ea3bcb16991591683
Стефан Максименко <cnefan@mail.ru>
2025-02-06 14:09:30 +0300

    feat: create recipe

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -20,9 +20,14 @@ class Recipe(models.Model):
     name = models.CharField(max_length=RECIPE_MAX_LENGTH)
     image = models.ImageField('Фото', upload_to='recipes_images')
     tags = models.ManyToManyField(Tag, related_name='recipes')
-    ingredients = models.ManyToManyField(Ingredient, related_name='recipes')
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient', related_name='recipes')
     text = models.TextField('Описание')
     cooking_time = models.SmallIntegerField('Время приготовления в минутах')
 
     def __str__(self):
         return self.name
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe_ingredient')
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe_ingredient')
+    amount = models.IntegerField()
--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -2,33 +2,58 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from drf_extra_fields.fields import Base64ImageField
 
+from ingredients.models import Ingredient
+from tags.models import Tag
 from tags.serializers import TagSerializer
 from ingredients.serializers import IngredientSerializer
-# from tags.models import Tag
 
-from .models import Recipe
+from .models import Recipe, RecipeIngredient
 
 User = get_user_model()
 
 
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(source='ingredient.id')
+    name = serializers.CharField(source='ingredient.name')
+    measurement_unit = serializers.CharField(source='ingredient.measurement_unit')
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ('id', 'name', 'measurement_unit', 'amount')
 
 class RecipeSerializer(serializers.ModelSerializer):
-    # tags = serializers.SlugRelatedField(slug_field='slug', many=True, queryset=Tag.objects.all())
+    tags = TagSerializer(many=True, read_only=True)
     image = Base64ImageField()
-    ingredients = IngredientSerializer(read_only=True, many=True)
-    author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    ingredients = RecipeIngredientSerializer(source='recipe_ingredient', read_only=True, many=True)
 
     class Meta:
         model = Recipe
         fields = '__all__'
-        # read_only_fields = ['author']
 
-    # def to_representation(self, instance):
-    #     representation = super().to_representation(instance)
-    #     representation['tags'] = TagSerializer(
-    #         data=instance.tags.all(), many=True
-    #     )
-    #     return representation
+    def create(self, validated_data):
+        tags_ids = self.initial_data.get('tags', [])
+        tags = Tag.objects.filter(id__in=tags_ids)
+        recipe = Recipe.objects.create(**validated_data)
+        recipe.tags.add(*tags)
 
-    # def to_internal_value(self, data):
-    #     return super().to_internal_value(data)
+        ingredients = self.initial_data.get('ingredients', [])
+
+        for ingredient in ingredients:
+            ingredient_object = Ingredient.objects.get(pk=ingredient['id'])
+            RecipeIngredient.objects.create(
+                recipe=recipe,
+                ingredient=ingredient_object,
+                amount=ingredient['amount'],
+            )
+
+        return recipe
+
+    # def update(self, instance, validated_data):
+    #     # Extract tag IDs from validated data
+    #     tags = validated_data.pop('tags', [])
+    #     # Update the recipe fields
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.save()
+    #     # Update the associated tags
+    #     instance.tags.set(tags)
+    #     return instance
